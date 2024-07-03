06c4d96191b40b62702084140749e8a6c7c24b0a
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:21:44 +0200

    Exercice 5: Boucles

---
+++ Exo5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exo5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Exercice 5: Boucles
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
