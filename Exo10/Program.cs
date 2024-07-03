7ecbebf716fa623924b45aa8152680ce626291b1
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:31:50 +0200

    Exercice 10: Concepts Orientés Objet

---
+++ Exo10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exo10
+{
+    public class Program
+    {
+        // Exercice 10: Concepts Orientés Objet
+        class Animal
+        {
+            public string Nom;
+            public void FaireDuBruit(string noise)
+            {
+                Console.WriteLine(noise);
+            }
+        }
+        public static void Main()
+        {
+            Animal cat = new Animal();
+            cat.FaireDuBruit("Miaou");
+        }
+    }
+}
