1ec06567e123fc98d791695306f3b9d5678b76d2
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:20:13 +0200

    // Exercice 4: Structures de Contrôle

---
+++ Exo4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exo4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Exercice 4: Structures de Contrôle
+            int nb;
+
+            Console.WriteLine("Entrez un nombre :");
+            string data = Console.ReadLine();
+            nb = int.Parse(data);
+
+            while (nb != 0)
+            {
+                if (nb > 0)
+                {
+                    Console.WriteLine("Le nombre : " + nb + " est positif");
+                }
+                else
+                {
+                    Console.WriteLine("Le nombre : " + nb + " est négatif");
+                }
+
+                Console.WriteLine("Entrez un nombre :");
+                data = Console.ReadLine();
+                nb = int.Parse(data);
+            }
+        }
+    }
+}
