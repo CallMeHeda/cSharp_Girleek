643602a2c028870019b6d3f4066d6dff5ba314c7
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:33:59 +0200

    Exercice 11: Gestion des Exceptions

---
+++ Exo11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exo11
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Exercice 11: Gestion des Exceptions
+            try
+            {
+                Console.WriteLine("Entrez le premier nombre :");
+                string data1 = Console.ReadLine();
+                double nb1 = double.Parse(data1);
+
+                Console.WriteLine("Entrez le deuxième nombre :");
+                string data2 = Console.ReadLine();
+                double nb2 = double.Parse(data2);
+
+                if (nb2 == 0)
+                {
+                    Console.WriteLine("Erreur : Division par zéro.");
+                }
+                else
+                {
+                    double result = nb1 / nb2;
+                    Console.WriteLine("Le résultat : " + result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
