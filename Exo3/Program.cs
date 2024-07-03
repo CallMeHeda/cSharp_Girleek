9f4cc35f034714573e88910575986edcc3c7e455
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:18:30 +0200

    Exercice 3 : Opérations sur les Variables

---
+++ Exo3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exo3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Exercice 3 : Opérations sur les Variables
+            int nb1 = 42;
+            int nb2 = 41;
+            int somme = nb1 + nb2;
+            Console.WriteLine(somme);
+        }
+    }
+}
