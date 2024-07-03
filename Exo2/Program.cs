24f4dc6540c1eb38f7a9499b3bb5177c89db6b55
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:16:01 +0200

    Exercice 2: Types de Données

---
+++ Exo2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exo2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Exercice 2: Types de Données
+            bool sunny = false;
+            Console.WriteLine(sunny);
+        }
+    }
+}
