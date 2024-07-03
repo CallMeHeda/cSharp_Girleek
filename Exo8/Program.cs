7b4ca921e99b6423091a5f16bc2f94e6644d70f3
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:27:04 +0200

    Exercice 8: Portée des Variables

---
+++ Exo8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exo8
+{
+    public class Program
+    {
+        // Exercice 8: Portée des Variables
+        // Variable Globale
+        static bool globale = true;
+        public static void Main()
+        {
+            // Variable Locale
+            bool sunny = false;
+
+            Console.WriteLine(globale);
+            Console.WriteLine(sunny);
+        }
+    }
+}
