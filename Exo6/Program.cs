9c934681533d9064d10d99f2cb383e9c2c9eb959
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:23:26 +0200

    Exercice 6: Introduction aux Fonctions

---
+++ Exo6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exo6
+{
+    public class Program
+    {
+        // Exercice 6: Introduction aux Fonctions
+        static void Afficher(int nb)
+        {
+            Console.WriteLine("Le nombre est : " + nb);
+        }
+        public static void Main()
+        {
+            Afficher(42);
+        }
+    }
+}
