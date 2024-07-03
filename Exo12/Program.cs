e46aa5bba1b3f1beffa68ce3a1e4c170249a38c2
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:35:23 +0200

    Exercice 12: Collections et Génériques

---
+++ Exo12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exo12
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Exercice 12: Collections et Génériques
+            List<int> listOfNumbers = new List<int>();
+            listOfNumbers.AddRange(new int[] { 2, 40, 8, 42, 105 }); // Add pour ajouter un seul nombre et AddRange pour en ajouter plusieurs à la fois
+
+            foreach (int number in listOfNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
