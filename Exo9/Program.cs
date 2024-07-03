76fde54a2e0fe949e003e1285274a5ad8803de36
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:29:54 +0200

    Exercice 9: Programmation Orientée Objet

---
+++ Exo9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exo9
+{
+    public class Program
+    {
+        // Exercice 9: Programmation Orientée Objet
+        class Animal
+        {
+            public string Nom;
+        }
+        public static void Main()
+        {
+            Animal lion = new Animal();
+            lion.Nom = "Simba";
+            Console.WriteLine(lion.Nom);
+        }
+    }
+}
