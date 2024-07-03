84d49468aafe5229d7c2bd1e1e3ba86b002c451a
FatimaElamri <fatimaelamri789@gmail.com>
2024-07-03 11:25:25 +0200

    Exercice 7: Fonctions et Méthodes

---
+++ Exo7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exo7
+{
+    public class Program
+    {
+        // Exercice 7: Fonctions et Méthodes
+        static int Somme(int nb1, int nb2)
+        {
+            return nb1 + nb2;
+        }
+        public static void Main()
+        {
+            Console.WriteLine(Somme(5, 10));
+        }
+    }
+}
